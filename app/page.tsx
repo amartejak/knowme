10385e58a5272e6a2207d7b263972457ac557660
amartejak <amarteja2@gmail.com>
2024-02-13 19:14:31 -0600

    Changed Experience

--- app/page.tsx
+++ app/page.tsx
@@ -60,82 +60,22 @@ export default function Home() {
 						<ul className="pl-8">
 							<li>
 								<ul className="list-disc pl-4">
-									<li className="list-none mb-2">Lead the development and implementation of innovative web solutions, contributing to the company&apos;s success in delivering high-quality products to clients.</li>
-									<li>
-										<strong>Front-End Development:</strong>
-										<ul className="pl-8">
-											<li>Spearheaded the front-end development of several client projects, utilizing advanced skills in HTML, CSS, and JavaScript.</li>
-											<li>Developed responsive and intuitive user interfaces using React.js, enhancing the overall user experience and engagement.</li>
-										</ul>
-									</li>
-									<li>
-										<strong>Back-End Development:</strong>
-										<ul className="pl-8">
-											<li>Designed and implemented robust server-side applications using Node.js, ensuring efficient data processing and system reliability.</li>
-											<li>Managed and optimized relational databases (MySQL) for improved performance and scalability.</li>
-										</ul>
-									</li>
-									<li>
-										<strong>API Development:</strong>
-										<ul className="pl-8">
-											<li>Engineered RESTful APIs to facilitate seamless communication between front-end and back-end systems, enabling efficient data exchange.</li>
-											<li>Implemented secure authentication mechanisms to safeguard user data and privacy.</li>
-										</ul>
-									</li>
-									<li>
-										<strong>Collaborative Development:</strong>
-										<ul className="pl-8">
-											<li>Worked collaboratively with cross-functional teams, participating in Agile/Scrum ceremonies to ensure project alignment with business goals.</li>
-											<li>Utilized Git for version control, leading to streamlined collaboration and efficient code management.</li>
-										</ul>
-									</li>
-									<li>
-										<strong>Deployment and Infrastructure:</strong>
-										<ul className="pl-8">
-											<li>Orchestrated deployment processes using AWS, automating server configurations and enhancing system scalability.</li>
-											<li>Monitored and maintained production environments, ensuring system availability and reliability.</li>
-										</ul>
-									</li>
-									<li>
-										<strong>Continuous Improvement:</strong>
-										<ul className="pl-8">
-											<li>Actively participated in ongoing professional development, staying abreast of emerging technologies and integrating them into project workflows.</li>
-											<li>Implemented testing strategies, including unit testing and integration testing, resulting in a reduction of post-release issues by 20%.</li>
-										</ul>
-									</li>
-									<li>
-										<strong>Client Interaction:</strong>
-										<ul className="pl-8">
-											<li>Engaged with clients to understand project requirements, provided technical insights, and delivered solutions that exceeded expectations.</li>
-										</ul>
-									</li>
+									<li>Developed and kept both front-end and back-end components of web applications using React, NextJs, TypeScript, Python, Django, and Flask frameworks.</li>
+									<li>Implemented responsive and user-friendly front-end interfaces using ReactJs, HTML5, CSS3, TypeScript and JavaScript.</li>
+									<li>Managed database systems including PostgreSQL, MySQL, MongoDB, and Elasticsearch, ensuring efficient data storage and retrieval.</li>
+									<li>Utilized Docker for containerization to streamline application deployment and management processes.</li>
+									<li>Orchestrate containerized applications using Kubernetes, ensuring scalability and reliability of production environments.</li>
+									<li>Implement continuous integration and continuous deployment (CI/CD) pipelines using Jenkins, easing automated testing and deployment processes.</li>
+									<li>Created and managed infrastructure as code (IaC) using Terraform, automating the provisioning and configuration of cloud resources.</li>
+									<li>Collaborated with cross-functional teams in an Agile environment, taking part in sprint planning, daily stand-ups, and retrospectives.</li>
+									<li>Developed interactive data visualizations using eCharts and D3.js to present complex data in a comprehensible manner.</li>
+									<li>Communicated effectively with team members and stakeholders to gather requirements, provided updates, and ensured alignment with project goals.</li>
+									<li>Proficient in data visualization tools like Tableau or Power BI to create insightful dashboards and reports for data-driven decision-making.</li>
+									<li>Proficient in big data analytics concepts, including data processing, storage, and analysis at scale.</li>
+									<li>Experienced working with data lakes, such as Apache Hadoop or AWS S3, to store and manage large volumes of structured and unstructured data.</li>
+									<li>Familiarity with Delta Lake, an open-source storage layer that brings reliability to data lakes, ensuring ACID transactions and data integrity.</li>
+									<li>Hands-on experience with Databricks, a unified analytics platform built on Apache Spark, for data engineering, machine learning, and analytics tasks.</li>
 								</ul>
-							</li>
-						</ul>
-					</div>
-					<div className="flex flex-col max-sm:flex-shrink-0 bg-transparent rounded-2xl p-2 m-2 shadow-lg transform dark:hover:bg-zinc-800 hover:bg-zinc-500 transition-all ease-in-out duration-500">
-						<h2 className="text-left mb-6 text-3xl font-semibold">Data Scientist
-						<p className="text-left text-sm bg-inherit">University of Kansas - Lawrence KS USA | Febraury 2023 - October 2023</p>
-						</h2>
-						
-						<ul className="list-disc  pl-12">
-							<li>
-							Leveraged Python, SQL, and machine learning libraries (e.g., TensorFlow, Scikit-learn) to analyze complex datasets and develop predictive models.
-							</li>
-							<li>
-							Led data collection, cleaning, and preprocessing efforts, ensuring data quality and readiness for analysis.
-							</li>
-							<li>
-							Conducted A/B experiments, resulting in data-driven decisions that optimized processes and improved outcomes.
-							</li>
-							<li>
-							Created impactful data visualizations using Tableau and Matplotlib to effectively communicate insights.
-							</li>
-							<li>
-							Collaborated cross-functionally with teams, defining project goals and delivering data-driven solutions.
-							</li>
-							<li>
-							Documented methodologies and findings, ensuring transparency and knowledge sharing.
 							</li>
 						</ul>
 					</div>
